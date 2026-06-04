51880d7d7275d4dbcf5aee00d227b3760a0bdd47
Joey.Kot <53976616+Joey-Kot@users.noreply.github.com>
2026-06-04 17:03:04 +0800

    Change project license to GPLv3

--- tests/test_response_transform.py
+++ tests/test_response_transform.py
@@ -1,3 +1,14 @@
+# Copyright (C) 2026 Joey Kot <joey.kot.x@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See <https://www.gnu.org/licenses/> for more details.
+
 import json
 import unittest
 
